ac7b11dff67c5d327bd36f478f9e4079e281b7dc
ooscarr <ooscarr@gmail.com>
2017-12-19 11:03:57 -0300

    Basta de bodyOnload

--- reproductor.js
+++ reproductor.js
@@ -3,6 +3,7 @@
  * CRÉDITOS (Inspiración)
  * Apple https://developer.apple.com/library/content/documentation/AudioVideo/Conceptual/Using_HTML5_Audio_Video/ControllingMediaWithJavaScript/ControllingMediaWithJavaScript.html#//apple_ref/doc/uid/TP40009523-CH3-SW5
  * Este tipo http://burnignorance.com/html-tips/how-to-play-multiple-videos-in-a-loop-using-html5-and-javascript/
+ * Este otro tipo https://stackoverflow.com/a/12460803/3369131
 */
 
 var videos = new Array();
@@ -14,6 +15,14 @@ videos[3]='videos/mp4/paranaltimelapse12i.mp4';
 
 var n_videos = videos.length;
 var i=0;
+
+function ejecuta()
+{
+	var elVideo = document.getElementsByTagName('video')[0];
+	elVideo.addEventListener('ended', cambiaVideo, false);
+}
+
+window[ addEventListener ? 'addEventListener' : 'attachEvent' ]( addEventListener ? 'load' : 'onload', ejecuta );
 
 function cambiaVideo()
 {
@@ -29,12 +38,4 @@ function dalePlay(videoNum)
 	document.getElementsByTagName('video')[0].setAttribute("src",videos[videoNum]);
 	document.getElementsByTagName('video')[0].load();
 	document.getElementsByTagName('video')[0].play();
-}
-
-function myAddListener()
-{
-	var elVideo = document.getElementsByTagName('video')[0];
-	
-	elVideo.addEventListener('ended', cambiaVideo, false);
-
 }
